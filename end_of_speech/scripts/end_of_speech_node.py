c39cb443f18b1cb238514c5ac35aa23f65462542
Christian Dondrup <c.dondrup@hw.ac.uk>
2018-04-10 13:45:08 +0100

    Bug fix and launch files

--- end_of_speech/scripts/end_of_speech_node.py
+++ end_of_speech/scripts/end_of_speech_node.py
@@ -6,7 +6,7 @@ from mummer_asr.msg import MummerAsr
 from naoqi_bridge_msgs.msg import AudioBuffer
 from end_of_speech.msg import EndOfSpeech
 
-SILENCE_THRESHOLD = 4
+SILENCE_THRESHOLD = 2
 
 class EndOfSpeech_node(object):
     def __init__(self):
@@ -23,9 +23,11 @@ class EndOfSpeech_node(object):
         if self.sub is None:
             self.sub = rospy.Subscriber("/noise_filter_node/result", AudioBuffer, self.audio_received, queue_size=1)
         self.last_end_time = msg.header.stamp.secs
-        self._google_utt_buff.extend([msg.final])
+        if msg.final != '':
+            self._google_utt_buff.append(msg.final)
         rospy.loginfo(msg.confidence)
-        self._google_conf_buff.extend([msg.confidence])
+        if msg.confidence > 0.0:
+            self._google_conf_buff.append(msg.confidence)
 
     def audio_received(self, msg):
         eos_msg = EndOfSpeech()
@@ -33,6 +35,7 @@ class EndOfSpeech_node(object):
 
         if (not self.audio_stamp - self.last_end_time <= SILENCE_THRESHOLD) and (not self.last_end_time == 0):
             eos_msg.final_utterance, eos_msg.confidence = self.create_final_utterance()
+            print "FINAL", eos_msg.final_utterance
             if eos_msg.confidence > 0.:
                 rospy.loginfo("Final utterance is '%s' with '%f' confidence" % (eos_msg.final_utterance, eos_msg.confidence))
 
@@ -48,7 +51,7 @@ class EndOfSpeech_node(object):
         utterance = ' '.join(self._google_utt_buff)
         self._google_utt_buff = []
         self._google_conf_buff = []
-        return utterance, avg_conf
+        return utterance.strip(), avg_conf
 
 
 if __name__ == '__main__':
